b67130a522771c25032399333e44527772a4799b
sam <samson.bassett@gmail.com>
2019-04-23 00:42:15 -0700

    dyes: plans to create if any.

--- src/main/java/com/lothrazar/simpledyes/ItemSimpleDye.java
+++ src/main/java/com/lothrazar/simpledyes/ItemSimpleDye.java
@@ -2,4 +2,15 @@ package com.lothrazar.simpledyes;
 
 import net.minecraft.item.Item;
 
-public class ItemSimpleDye extends Item {}
+public class ItemSimpleDye extends Item {
+  //new blue dye item
+  //smelt from lapis into blue dye
+  //remove ore dict from lapis
+  //remove all lapis recies EXCEPT:
+  //keep lapis block recipe 
+  // 
+  //new brown dye recipe 
+  //remove oredict
+  //smelt from cocoa
+  //keep only cookie recipe
+}
