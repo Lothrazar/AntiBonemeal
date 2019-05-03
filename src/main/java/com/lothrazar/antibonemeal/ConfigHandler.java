36d07d5c7705fbb68f8180a33fc53159a2cf3f26
sam <samson.bassett@gmail.com>
2019-05-02 21:02:52 -0700

    1.0.0 oops save

--- src/main/java/com/lothrazar/antibonemeal/ConfigHandler.java
+++ src/main/java/com/lothrazar/antibonemeal/ConfigHandler.java
@@ -18,5 +18,6 @@ public class ConfigHandler {
   public static void loadConfig(Configuration config) {
     tooltips = config.getBoolean("ShowTooltips", ModAnti.MODID, true, "Show tooltips from this mod");
     grassMidnight = config.getBoolean("GrassMidnight", ModAnti.MODID, true, "Allows bonemeal to work during midnight, but only on grass (to grow flowers, maybe a rare firework).  If false it just never works on grass just like other blocks.");
+    config.save();
   }
 }
