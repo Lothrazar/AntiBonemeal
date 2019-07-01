82b1f021e8a96897508098c45efcb45c26296e91
sam <samson.bassett@gmail.com>
2019-07-01 12:43:42 -0700

    fix lang, ignore out. fix particles. add tooltip color

--- src/main/java/com/lothrazar/antibonemeal/DyeEventHandler.java
+++ src/main/java/com/lothrazar/antibonemeal/DyeEventHandler.java
@@ -1,10 +1,12 @@
 package com.lothrazar.antibonemeal;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.FireworkRocketEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,33 +18,48 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class DyeEventHandler {
 
   final static int hours24 = 24000;
+  final static int MIDNIGHT = 18;
 
   @SubscribeEvent
   public void onBone(BonemealEvent event) {
+    World world = event.getWorld();
+    double x = event.getPos().getX() + .5;
+    double y = event.getPos().getY();
+    double z = event.getPos().getZ() + .5;
     if (ConfigHandler.grassMidnight()
         && event.getBlock().getBlock().equals(Blocks.GRASS)) {
-      World world = event.getWorld();
-      long worldtime = world.getGameTime();
-      long timeWithinDay = worldtime % hours24;
+      long worldTime = world.getGameTime();
+      long timeWithinDay = worldTime % hours24;
       int hour = ((int) timeWithinDay) / 1000;
-      if (hour < 17 || hour > 19) {
+      if (hour < MIDNIGHT - 2 || hour > MIDNIGHT + 2) {
         //hour 18 is midnight in MC world
+        doSmoke(world, z, y, x);
         event.setCanceled(true);
       }
       else {// NOT CANCELLED
         //meaning you can bonemeal grass now wooo
-        //particles
-        //fireworks 
-        if (world.rand.nextDouble() < 0.05) {
-          Entity rocket = new FireworkRocketEntity(world, new ItemStack(Items.FIREWORK_ROCKET), event.getEntityPlayer());
-          rocket.setPosition(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
-          world.addEntity(rocket);
-        }
+        doFireworks(event.getEntityPlayer(), world, z, y, x);
       }
     }
     else {
       //either im not grass. or i am grass and config is turned off
+      doSmoke(world, z, y, x);
       event.setCanceled(true);
+    }
+  }
+
+  private void doFireworks(PlayerEntity player, World world, double z, double y, double x) {
+    if (world.rand.nextDouble() < 0.05) {
+      Entity rocket = new FireworkRocketEntity(world, new ItemStack(Items.FIREWORK_ROCKET), player);
+      rocket.setPosition(x, y, z);
+      world.addEntity(rocket);
+    }
+  }
+
+  private void doSmoke(World world, double z, double y, double x) {
+    System.out.println(x + " true, " + world.isRemote);
+    for (int i = 0; i < 20; i++) {
+      world.addParticle(ParticleTypes.SMOKE, x, y, z, 0.0D, 0.2D, 0.0D);
     }
   }
 
@@ -51,8 +68,7 @@ public class DyeEventHandler {
   public void onItemTooltipEvent(ItemTooltipEvent event) {
     if (ConfigHandler.tooltipsEnabled() &&
         event.getItemStack().getItem() == Items.BONE_MEAL) {
-      String tip = I18n.format("item.bone_meal.tooltip");
-      event.getToolTip().add(new TranslationTextComponent(tip));
+      event.getToolTip().add(new TranslationTextComponent(ModAnti.MODID + ".item.bone_meal.tooltip").applyTextStyle(TextFormatting.GRAY));
     }
   }
 }
