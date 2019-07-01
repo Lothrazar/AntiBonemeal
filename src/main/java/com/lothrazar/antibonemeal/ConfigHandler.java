0c5ef88db455ef283a8a9ab3cd269dfaa07047f9
sam <samson.bassett@gmail.com>
2019-07-01 11:01:19 -0700

    cleaning up and fixing lang

--- src/main/java/com/lothrazar/antibonemeal/ConfigHandler.java
+++ src/main/java/com/lothrazar/antibonemeal/ConfigHandler.java
@@ -1,25 +1,21 @@
 package com.lothrazar.antibonemeal;
-
-import net.minecraftforge.common.ForgeConfigSpec;
-
 public class ConfigHandler {
 
-
-  private static boolean tooltips;
-  private static boolean grassMidnight;
-
+  private static boolean tooltips = true;
+  private static boolean grassMidnight = true;
 
   public static boolean tooltipsEnabled() {
+    tooltips = true
+    ;
     return tooltips;
   }
 
   public static boolean grassMidnight() {
     return grassMidnight;
   }
-
-//  public static void loadConfig(Configuration config) {
-//    tooltips = config.getBoolean("ShowTooltips", ModAnti.MODID, true, "Show tooltips from this mod");
-//    grassMidnight = config.getBoolean("GrassMidnight", ModAnti.MODID, true, "Allows bonemeal to work during midnight, but only on grass (to grow flowers, maybe a rare firework).  If false it just never works on grass just like other blocks.");
-//    config.save();
-//  }
+  //  public static void loadConfig(Configuration config) {
+  //    tooltips = config.getBoolean("ShowTooltips", ModAnti.MODID, true, "Show tooltips from this mod");
+  //    grassMidnight = config.getBoolean("GrassMidnight", ModAnti.MODID, true, "Allows bonemeal to work during midnight, but only on grass (to grow flowers, maybe a rare firework).  If false it just never works on grass just like other blocks.");
+  //    config.save();
+  //  }
 }
--- src/main/java/com/lothrazar/antibonemeal/DyeEventHandler.java
+++ src/main/java/com/lothrazar/antibonemeal/DyeEventHandler.java
@@ -1,17 +1,17 @@
 package com.lothrazar.antibonemeal;
-
-import javafx.geometry.Side;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.FireworkRocketEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraft.entity.item.FireworkRocketEntity;
 
 public class DyeEventHandler {
 
@@ -27,7 +27,6 @@ public class DyeEventHandler {
       int hour = ((int) timeWithinDay) / 1000;
       if (hour < 17 || hour > 19) {
         //hour 18 is midnight in MC world
-      //  ModAnti.LOGGER.error("YPE");
         event.setCanceled(true);
       }
       else {// NOT CANCELLED
@@ -38,36 +37,22 @@ public class DyeEventHandler {
           Entity rocket = new FireworkRocketEntity(world, new ItemStack(Items.FIREWORK_ROCKET), event.getEntityPlayer());
           rocket.setPosition(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
           world.addEntity(rocket);
-
         }
       }
     }
     else {
       //either im not grass. or i am grass and config is turned off
-    //ModAnti.LOGGER.error("!!asdfasdfYPEasdfsde");
-     event.setCanceled(true);
+      event.setCanceled(true);
     }
   }
-//
-//  @SuppressWarnings({ "deprecation", "incomplete-switch" })
-//  @OnlyIn(Dist.CLIENT)
-//  @SubscribeEvent
-//  public void onItemTooltipEvent(ItemTooltipEvent event) {
-//    if (ConfigHandler.tooltipsEnabled() &&
-//        event.getItemStack().getItem() == Items.DYE) {
-//      int meta = event.getItemStack().getMetadata();
-//      EnumDyeColor value = EnumDyeColor.byDyeDamage(meta);
-//      switch (value) {
-//        case BLACK:
-//        case GREEN:
-//        case RED:
-//        case YELLOW:
-//        case WHITE:
-//        case BROWN:
-//        case BLUE:
-//          event.getToolTip().add(I18n.translateToLocal("item.dyePowder." + value.name().toLowerCase() + ".tooltip"));
-//        break;
-//      }
-//    }
-//  }
+
+  @OnlyIn(Dist.CLIENT)
+  @SubscribeEvent
+  public void onItemTooltipEvent(ItemTooltipEvent event) {
+    if (ConfigHandler.tooltipsEnabled() &&
+        event.getItemStack().getItem() == Items.BONE_MEAL) {
+      String tip = I18n.format("item.bone_meal.tooltip");
+      event.getToolTip().add(new TranslationTextComponent(tip));
+    }
+  }
 }
