e185e31b98d48c16e11230dd2bb8f149b847eb2a
lothrazar <samson.bassett@gmail.com>
2020-10-01 17:44:16 -0700

    1.16.3 bump release

--- src/main/java/com/lothrazar/antibonemeal/ModAnti.java
+++ src/main/java/com/lothrazar/antibonemeal/ModAnti.java
@@ -1,22 +1,15 @@
 package com.lothrazar.antibonemeal;
-import com.lothrazar.antibonemeal.setup.ClientProxy;
-import com.lothrazar.antibonemeal.setup.IProxy;
-import com.lothrazar.antibonemeal.setup.ServerProxy;
+
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
- 
+
 @Mod(ModAnti.MODID)
 public class ModAnti {
 
   public static final String MODID = "antibonemeal";
-//  public static final IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
-//  public static final Logger LOGGER = LogManager.getLogger();
 
   public ModAnti() {
     // Register the setup method for modloading
@@ -27,6 +20,5 @@ public class ModAnti {
     ConfigHandler.loadConfig(ConfigHandler.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve(MODID + ".toml"));
   }
 
-  private void setup(final FMLCommonSetupEvent event) {
-  }
+  private void setup(final FMLCommonSetupEvent event) {}
 }
--- src/main/java/com/lothrazar/antibonemeal/setup/IProxy.java
+++
@@ -1,7 +0,0 @@
-package com.lothrazar.antibonemeal.setup;
-
-import net.minecraft.world.World;
-
-public interface IProxy {
-	World getClientWorld();
-}
